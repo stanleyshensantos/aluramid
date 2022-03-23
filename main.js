14cf1c5c1bcff82dd0a7070873427cb0a28051ca
Staney Shen <stanley_shen@hotmail.com>
2022-03-23 14:13:13 -0300

    https://github.com/stanleyshensantos/aluramid

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-document.querySelector('.tecla_pom');
+// Pom ......................................
+function tocaSomPom (){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const lsitaDeTeclas = document.querySelectorAll('.tecla');
+
+lsitaDeTeclas[0].onclick = tocaSomPom;
+
