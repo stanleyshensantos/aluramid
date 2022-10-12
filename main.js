536478a1850c414e2197c1addb975ba1d5e21783
Staney Shen <stanley_shen@hotmail.com>
2022-10-12 18:07:12 -0300

    https://github.com/stanleyshensantos/aluramid

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 
-function tocaSom (idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio){
+   const elemento = document.querySelector(seletorAudio);
+       
+    if (elemento  && elemento.localName === 'audio'){
+            elemento.play();
+    }
+    else{
+        alert('Elemento não encontrado ou seletor invalido');
+    }
+    
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -14,5 +22,14 @@ for( let contador = 0; contador < listaDeTeclas.length; contador++) {
 
   tecla.onclick = function () {
         tocaSom(idAudio);
+    }
+    tecla.onkeydown = function (evento){
+        
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
     }
 }
